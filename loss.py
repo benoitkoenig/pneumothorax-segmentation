61c96c8087c09c5ae330eab4c98ef0c0eb69b074
Benoit Koenig <koenigbenoit47@gmail.com>
2019-07-28 15:44:13 -0600

    Move loss calculation in loss.py

---
+++ loss.py
@@ -0,0 +1,16 @@
+import numpy as np
+import tensorflow as tf
+from tensorflow.nn import sparse_softmax_cross_entropy_with_logits
+
+from pneumothorax_segmentation.constants import image_size
+from pneumothorax_segmentation.params import disease_pixel_weight
+
+def calculate_loss(predicted_logits, true_mask):
+    "Calculates loss. predicted_logits are the output of the Unet model. True_mask is of shape (image_size, image_size)"
+    resized_predicted_logits = tf.image.resize(predicted_logits, (image_size, image_size))
+    reshaped_true_mask = np.reshape(true_mask, (1, image_size, image_size))
+
+    pixel_loss = sparse_softmax_cross_entropy_with_logits(logits=resized_predicted_logits, labels=reshaped_true_mask)
+    pixel_loss = tf.multiply(disease_pixel_weight * reshaped_true_mask, pixel_loss)
+
+    return tf.reduce_sum(pixel_loss)
--- train.py
+++ train.py
@@ -1,12 +1,12 @@
 import numpy as np
 import tensorflow as tf
 from tensorflow.keras.optimizers import Adam
-from tensorflow.nn import sparse_softmax_cross_entropy_with_logits
 
 tf.compat.v1.enable_eager_execution() # Remove when switching to tf2
 
-from pneumothorax_segmentation.constants import image_size, tf_image_size
-from pneumothorax_segmentation.params import disease_pixel_weight, learning_rate
+from pneumothorax_segmentation.constants import image_size
+from pneumothorax_segmentation.loss import calculate_loss
+from pneumothorax_segmentation.params import learning_rate
 from pneumothorax_segmentation.preprocess import get_all_images_list, get_dicom_data, get_true_mask, format_pixel_array_for_unet
 from pneumothorax_segmentation.tracking import save_data
 from pneumothorax_segmentation.unet import Unet
@@ -19,29 +19,18 @@ def train():
     images_list = get_all_images_list("train")
     for (index, (filepath, filename)) in enumerate(images_list):
         dicom_data = get_dicom_data(filepath)
-        true_mask = get_true_mask(filename)
-
         image = format_pixel_array_for_unet(dicom_data.pixel_array)
-        true_mask = np.reshape(true_mask, (1, image_size, image_size))
+        true_mask = get_true_mask(filename)
 
         def get_loss():
             predicted_logits = unet(image)
-
             save_data(index, predicted_logits, true_mask)
-
-            resized_predicted_logits = tf.image.resize(predicted_logits, (image_size, image_size))
-            pixel_loss = sparse_softmax_cross_entropy_with_logits(logits=resized_predicted_logits, labels=true_mask)
-            pixel_loss = tf.multiply(disease_pixel_weight * true_mask, pixel_loss)
-
-            return tf.reduce_sum(pixel_loss)
+            return calculate_loss(predicted_logits, true_mask)
 
         opt.minimize(get_loss, [unet.trainable_weights])
 
         if index % 100 == 99:
-            print("Index %s processed - Weights saved" % index)
             unet.save_weights("./weights/unet")
-        else:
-            print("Index %s processed" % index)
 
     print("Training done over %s images. Saving final weights" % len(images_list))
     unet.save_weights("./weights/unet")
