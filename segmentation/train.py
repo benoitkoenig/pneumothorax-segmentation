1d88b1ff6f72d494c21f89461ab26bb84913dd10
Benoit Koenig <koenigbenoit47@gmail.com>
2019-08-10 20:06:08 -0600

    Add a workaround in segmentation/train to handle the memory leak, althought the workaround is not an efficient solution and can only be considered as temporary

--- segmentation/train.py
+++ segmentation/train.py
@@ -1,3 +1,4 @@
+from keras import backend as K
 from keras.callbacks import ModelCheckpoint
 from keras.optimizers import Adam
 from segmentation_models import Unet
@@ -6,26 +7,31 @@ import tensorflow as tf
 from pneumothorax_segmentation.constants import folder_path, image_size, tf_image_size
 from pneumothorax_segmentation.segmentation.loss import calculate_loss
 from pneumothorax_segmentation.segmentation.params import learning_rate, steps_per_epoch, epochs
-from pneumothorax_segmentation.segmentation.training_generator import training_generator, generator_length
-
-graph = tf.compat.v1.get_default_graph()
-
-if (generator_length != steps_per_epoch * epochs):
-    print("\n Warning: The generator's length is different from steps_per_epoch * epochs: %s != %s * %s" % (generator_length, steps_per_epoch, epochs))
+from pneumothorax_segmentation.segmentation.training_generator import training_generator
 
 def train():
-    unet = Unet(
-        "resnet34",
-        encoder_weights="imagenet",
-        classes=1,
-        activation="sigmoid",
-        input_shape=(tf_image_size, tf_image_size, 3),
-    )
-    unet.compile(optimizer=Adam(lr=learning_rate), loss=calculate_loss)
-
     model_checkpoint = ModelCheckpoint(folder_path + "/weights/unet.hdf5")
-
-    gen = training_generator(graph)
-    unet.fit_generator(gen, steps_per_epoch=steps_per_epoch, epochs=epochs, callbacks=[model_checkpoint])
+    for epoch_index in range(epochs):
+        # Why iterating avor epochs instead of passing them correctly as a parameter of fit_generator?
+        # The problem is that I need to clean the graph regularly, or I will get a memory error
+        # And I did not find a way to clean the memory from used operations without resetting the whole graph
+        # But resetting the whole graph also requires to re-load the wholes weights
+        # This is obviously not an acceptable long-term solution. To see the issue on github:
+        # https://github.com/tensorflow/tensorflow/issues/31419
+        graph = tf.Graph()
+        K.clear_session()
+        gen = training_generator(graph, starting_index=epoch_index * steps_per_epoch)
+        with graph.as_default():
+            unet = Unet(
+                "resnet34",
+                encoder_weights="imagenet",
+                classes=1,
+                activation="sigmoid",
+                input_shape=(tf_image_size, tf_image_size, 3),
+            )
+            if (epoch_index != 0):
+                unet.load_weights(folder_path + "/weights/unet.hdf5")
+            unet.compile(optimizer=Adam(lr=learning_rate), loss=calculate_loss)
+            unet.fit_generator(gen, steps_per_epoch=steps_per_epoch, epochs=1, callbacks=[model_checkpoint])
 
 train()
