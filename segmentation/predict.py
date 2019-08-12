698aae7e7a9e35d984f133e4ec95bf8d32bf15c0
Benoit Koenig <koenigbenoit47@gmail.com>
2019-08-11 20:57:46 -0600

    Update segmentation.train to save the whole model, and not only the weights

--- segmentation/predict.py
+++ segmentation/predict.py
@@ -1,16 +1,9 @@
-from segmentation_models import Unet
+from keras.models import load_model, save_model
 import tensorflow as tf
 
 from pneumothorax_segmentation.constants import folder_path, tf_image_size
 
-unet = Unet(
-    'resnet34',
-    encoder_weights='imagenet',
-    classes=1,
-    activation="sigmoid",
-    input_shape=(tf_image_size, tf_image_size, 3),
-)
-unet.load_weights(folder_path + "/weights/unet.hdf5")
+unet = load_model(folder_path + "/weights/unet.hdf5", compile=False)
 
 def get_prediction(image):
     return unet.predict(image, steps=1)
--- segmentation/train.py
+++ segmentation/train.py
@@ -1,5 +1,6 @@
 from keras import backend as K
 from keras.callbacks import ModelCheckpoint
+from keras.models import load_model, save_model
 from keras.optimizers import Adam
 from segmentation_models import Unet
 import tensorflow as tf
@@ -9,10 +10,11 @@ from pneumothorax_segmentation.segmentation.loss import calculate_loss
 from pneumothorax_segmentation.segmentation.params import learning_rate, steps_per_epoch, epochs
 from pneumothorax_segmentation.segmentation.training_generator import training_generator
 
+file_path = folder_path + "/weights/unet.hdf5"
+
 def train():
-    model_checkpoint = ModelCheckpoint(folder_path + "/weights/unet.hdf5")
     for epoch_index in range(epochs):
-        # Why iterating avor epochs instead of passing them correctly as a parameter of fit_generator?
+        # Why iterating over epochs instead of passing them correctly as a parameter of fit_generator?
         # The problem is that I need to clean the graph regularly, or I will get a memory error
         # And I did not find a way to clean the memory from used operations without resetting the whole graph
         # But resetting the whole graph also requires to re-load the wholes weights
@@ -22,16 +24,18 @@ def train():
         K.clear_session()
         gen = training_generator(graph, starting_index=epoch_index * steps_per_epoch)
         with graph.as_default():
-            unet = Unet(
-                "resnet34",
-                encoder_weights="imagenet",
-                classes=1,
-                activation="sigmoid",
-                input_shape=(tf_image_size, tf_image_size, 3),
-            )
-            if (epoch_index != 0):
-                unet.load_weights(folder_path + "/weights/unet.hdf5")
-            unet.compile(optimizer=Adam(lr=learning_rate), loss=calculate_loss)
-            unet.fit_generator(gen, steps_per_epoch=steps_per_epoch, epochs=1, callbacks=[model_checkpoint])
+            if (epoch_index == 0):
+                unet = Unet(
+                    "resnet34",
+                    encoder_weights="imagenet",
+                    classes=1,
+                    activation="sigmoid",
+                    input_shape=(tf_image_size, tf_image_size, 3),
+                )
+                unet.compile(optimizer=Adam(lr=learning_rate), loss=calculate_loss)
+            else:
+                unet = load_model(file_path, custom_objects={"calculate_loss": calculate_loss})
+            unet.fit_generator(gen, steps_per_epoch=steps_per_epoch, epochs=1)
+            save_model(unet, file_path)
 
 train()
