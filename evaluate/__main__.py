9b4e5c89caf331f615265f168ecde1d3c8ff2937
Benoit Koenig <koenigbenoit47@gmail.com>
2019-08-24 06:55:18 -0600

    Add wrong diagnosis area to evaluation, to have information about no-mask pictures

--- evaluate/__main__.py
+++ evaluate/__main__.py
@@ -1,6 +1,7 @@
 import numpy as np
 import tensorflow as tf
 
+from pneumothorax_segmentation.constants import image_size
 from pneumothorax_segmentation.preprocess import get_all_images_list, get_true_mask, get_image_label
 from pneumothorax_segmentation.postprocess import apply_threshold_to_preds
 from pneumothorax_segmentation.segmentation.predict import get_prediction
@@ -15,17 +16,22 @@ def evaluate():
     images_list = get_all_images_list("train")
     len_images_list = len(images_list)
     dices_coefficients = [[], [], []]
+    wrong_diagnosis_areas = [[], [], []]
     for (fileindex, (filepath, filename)) in enumerate(images_list):
         label = get_image_label(filename)
-        if (label == 1):
-            true_mask = get_true_mask(filename)
-            predictions = get_prediction(filepath)
-
-            print("\n%s/%s Avg Dice Coef for threshold" % (fileindex, len_images_list))
-            for i in range(3):
-                preds = apply_threshold_to_preds(predictions, thresholds[i])
-                dice_coef = calculate_dice_coefficient(preds, true_mask)
+        true_mask = get_true_mask(filename)
+        predictions = get_prediction(filepath)
+        for i in range(3):
+            predicted_mask = apply_threshold_to_preds(predictions, thresholds[i])
+            if (label == 1):
+                dice_coef = calculate_dice_coefficient(predicted_mask, true_mask)
                 dices_coefficients[i].append(dice_coef)
-                print("%s %s" % (thresholds[i], np.mean(dices_coefficients[i])))
+            else:
+                area = np.sum(predicted_mask) / (image_size ** 2)
+                wrong_diagnosis_areas[i].append(area)
+
+        print("\n%s/%s Avg dice coef and wrong diagnosis area for threshold" % (fileindex, len_images_list))
+        for i in range(3):
+            print("%s\t%s\t%s" % (thresholds[i], np.mean(dices_coefficients[i]), np.mean(wrong_diagnosis_areas[i])))
 
 evaluate()
